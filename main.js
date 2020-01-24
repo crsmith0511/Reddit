f24c3688d222921b514d437c32889044500d83e9
Casey Smith <caseysmith@Caseys-MacBook-Pro.local>
2020-01-24 14:14:10 -0500

    HTML displays your name text field.

--- main.js
+++ main.js
@@ -3,29 +3,29 @@ var posts = []
 
 
 //when the post button gets clicked id="submit-post"
-var createNewPost = $('#submit-post').on('click', function(){
+var addNewPost = $('#submit-post').on('click', function(){
     //grab text post info
-    var submittedPost = $('#post-text').val()
+    var submittedPostText = $('#post-text').val()
     //grab your name info
-    var submittedName = $('#your-name').val()
+    var submittedYourName = $('#your-name').val()
 
     //check to see if both text fields are blank.
-    if(submittedPost === '' && submittedName === ''){
+    if(submittedPostText === '' && submittedYourName === ''){
         alert('Please fill in all fields before clicking Post.')
         //check to see if post text is blank
-    } else if(submittedPost === '' && submittedName){
+    } else if(submittedPostText === '' && submittedYourName){
         alert('Please fill out the post text')
         //checks to see if your name is blank
-    } else if(submittedPost && submittedName === ''){
+    } else if(submittedPostText && submittedYourName === ''){
         alert('Please fill in your name.')
     } else {
         //store it in an object
-        var post = {
-            postText: submittedPost,
-            yourName: submittedName
+        var newSubmittedPost = {
+            postText: submittedPostText,
+            yourName: submittedYourName
         }
-        console.log(post)
-        posts.push(post)
+        console.log(newSubmittedPost)
+        posts.push(newSubmittedPost)
     }
 
     //push that object ot the posts object
