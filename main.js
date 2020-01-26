7d9eda4402ab57f8c728a707efcd947c232290fa
Casey Smith <caseysmith@Caseys-MacBook-Pro.local>
2020-01-26 16:15:56 -0500

    Posts are displayed using<a>, <p>. Comment and Remove are grabbing the clicked ID or index number.

--- main.js
+++ main.js
@@ -32,22 +32,77 @@ var addNewPost = $('#submit-post').on('click', function(){
 })
 
 var renderPosts = function(){
+    //finds the <div> that template or each post will be appended to. 
+    // In this case it is the div that has the class of post-thread view.
     var $postThreadView = $('.post-thread-view')
 
+    //This is emptying out the div container so the loop can add each new post and have no dups.
     $postThreadView.empty()
 
- 
-        for(let i = 0; i < posts.length; i++){
-            var $template =
-            '<tr class="post-text-view">'
-          + '  <td class="remove-post" data-post-number="' + i + '">'+'<a>' + 'Remove' + '</a>' + '</td>'
-          + '  <td class="comment-on-post" data-post-number="' + i + '">' +'<a>' + 'Comment' + '</a>' + '</td>'
-          + '  <td class="post-text" data-post-number="' + i + '">' + posts[i].postText + '</td>'
-          + '  <td class="post-creator" data-post-number="' + i + '">' + 'Posted By: ' + posts[i].postCreator + '</td>'
-          + '</tr>'
-          ;  
-          $postThreadView.append($template)
-        }
+    for(let i = 0; i < posts.length; i++){
+        var $template =
+        '<div class="post-container" id="' + i + '"> '
+        +   '<a class="remove-post" data-remove="' + i + '">' + 'Remove' + '</a> '
+        +   '<a class="comment-on-post" data-comment="' + i + '">' + 'Comment' + '</a>'
+        +   '<p class="post-text">' + posts[i].postText + '</p>'
+        +       '<div class="comment-container" data-number="' + i + '">'
+        +           '<ul class="comment-section" data-comment-container="' + i + '">' + '</ul>'
+        +           '<input id="comment-text" type="text" class="comment-control" placeholder="Comment Text">' + '</input>'
+        +           '<input id="user-name" type="text" class="comment-control" placeholder="User Name">' + '</input>'
+        +           '<button id="submit-comment" data-submit="' + i + '">' + 'Post Comment' + '</button>'
+        +       '</div>'
+        +   '<p class="post-creator">' + 'Post By: ' + posts[i].postCreator  + '</p>'
+        +'</div>';
+
+      $postThreadView.append($template)
+    }
 }
+console.log(posts)
+
+// when the post button gets clicked id="submit-post"
+var addNewComment = $('.post-container').on('click', '#submit-comment', function(){
+    var postId = $(this).data('submit')
+    
+    console.log(index)
+    //grabs comment text
+    var submittedCommentText = $('#comment-text').val()
+    //grabs user name
+    var submittedUserName = $('#user-name').val()
+
+    //check to see if both text fields are blank.
+    if(submittedCommentText === '' && submittedUserName === ''){
+        alert('Please fill in all fields before clicking Post.')
+        //check to see if comment text is blank
+    } else if(submittedCommentText === '' && submittedUserName){
+        alert('Please fill out the post text')
+        //checks to see if user name is blank
+    } else if(submittedCommentText && submittedUserName === ''){
+        alert('Please fill in your name.')
+    } else {
+        //store it in an object if ALL fields are filled out, so no objects get made with undefined values.
+        var newCommentPost = {
+            commentText: submittedCommentText,
+            commentCreator: submittedUserName,
+        }
+       console.log(newCommentPost)
+        //need to push object to the posts[correct index].commentThread
+    }
+    
+})
 
 
+
+$(".post-thread").on('click', '.remove-post', function(){
+    var removePostid= $(this).data('remove')
+    // Need to remove post from the posts array by index
+    // re-render posts?
+   
+})
+
+
+$(".post-thread").on('click', '.comment-on-post', function(){
+    var commentOnid = $(this).data('comment')
+    
+    $('.comment-container').eq(commentOnid).toggle()
+
+})
